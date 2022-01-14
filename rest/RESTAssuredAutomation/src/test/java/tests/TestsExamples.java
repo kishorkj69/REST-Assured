60148ecb7a8f6a02a653e6c9c8503597d9103468
Kishor Kumar Jha <36334841+kishorkj69@users.noreply.github.com>
2022-01-14 15:31:08 +0530

    Add files via upload

---
+++ rest/RESTAssuredAutomation/src/test/java/tests/TestsExamples.java
@@ -0,0 +1,58 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*; 
+
+public class TestsExamples {
+	
+	@Test
+	public void test_1() {
+		
+		
+		Response response = get("https://reqres.in/api/users?page=2");
+		
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		System.out.println(response.getBody().asString());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type")) ;
+		
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, 200);
+		
+				
+		
+	}
+	
+	@Test
+	public void test_2() {
+		
+		baseURI="https://reqres.in/api";
+		
+		given().
+			get("/users?page=2").
+		then().
+			statusCode(200).
+			body("data[1].id",equalTo(8))
+			.log().all();
+	}
+	
+	@Test
+	public void test_3() {
+		
+		Response response= get("https://reqres.in/api/users/23");
+		
+		System.out.println(response.getBody().asString());
+		
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(statusCode, 404);
+		
+	}
+
+}
